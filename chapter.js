2008b0cadf07af0a55a7a894f7d2de1c7086d96f
Paulux <45801364+Paulux06@users.noreply.github.com>
2020-12-30 14:24:43 +0100

    added text and script for chapter 1

---
+++ chapter.js
@@ -0,0 +1,86 @@
+function loadChapter(index=1) {
+    switch (index) {
+        case 1:
+            setupchapter1()
+            break;
+        case 2:
+            //SOUNDS.CHAPTER_2.NARATIVE.play();
+            break;
+        case 3:
+            //SOUNDS.CHAPTER_3.NARATIVE.play();
+            break;
+        case 4:
+            //SOUNDS.CHAPTER_4.NARATIVE.play();s
+            break;
+    
+        default:
+            break;
+    }
+}
+
+function openDialog() {
+    let container = document.getElementById("chapters-text-container");
+
+    container.style.display = "block";
+    container.style.maxHeight = "10vh";
+}
+
+/**
+ * Set the dialog text to the argument
+ * @param {string} text 
+ */
+function setDialogText(text) {
+    let container = document.getElementById("chapters-text-container");
+    let content = document.getElementById("chapters-text-content");
+    let hider_x = document.getElementById("chapters-text-hider-x");
+    let hider_y = document.getElementById("chapters-text-hider-y");
+    content.innerHTML = text;
+    let bounds = container.getBoundingClientRect();
+    let height = bounds.height;
+    let width = bounds.width;
+    
+    for (let i = 0; i < Math.ceil(height/CONSTANTS.DIALOG_TEXT_HEIGHT); i++) {
+        setTimeout(() => {
+            hider_x.style.transition = "none";
+            hider_y.style.transition = "none";
+            hider_x.style.left = "0px";
+            hider_x.style.width = width+"px";
+            hider_x.style.top = (i*CONSTANTS.DIALOG_TEXT_HEIGHT)+"px";
+            hider_x.style.height = (height-i*CONSTANTS.DIALOG_TEXT_HEIGHT)+"px";
+            hider_y.style.top = (i*CONSTANTS.DIALOG_TEXT_HEIGHT-height+(i+1)*CONSTANTS.DIALOG_TEXT_HEIGHT)+"px";
+            hider_y.style.height = height+"px";
+            setTimeout(() => {
+                hider_x.style.transition = CONSTANTS.HIDER_X_TRANSITION;
+                hider_y.style.transition = CONSTANTS.HIDER_Y_TRANSITION;
+                hider_x.style.left = width.toString()+"px";
+                hider_x.style.width = "0px";
+            }, 100);
+        }, i*1400);
+    }
+}
+
+function closeDialog() {
+    let container = document.getElementById("chapters-text-container");
+
+    container.style.maxHeight = "0vh";
+    setTimeout(()=>{container.style.display = "none";}, CONSTANTS.ANIMATION_DELAY);
+}
+
+function setupchapter1() {
+    SOUNDS.CHAPTER_1.NARATIVE.play();
+    openDialog()
+    setTimeout(()=>{setDialogText("J’avais 15 ans en 1939.")}, 550);
+    setTimeout(()=>{setDialogText("Nous vivions dans un pays antisémite, sous l’occupation allemande, dans le ghetto juif.")}, 3240);
+    setTimeout(()=>{setDialogText("Séparés des autres.")}, 7710);
+    setTimeout(()=>{setDialogText("Les soldats de la Gestapo patrouillaient dans le ghetto, à la recherche de gens comme nous.")}, 9160);
+    setTimeout(()=>{setDialogText("Nous savions que s’ils nous trouvaient chez nous, nous devions quitter la ville.")}, 13700);
+    setTimeout(()=>{setDialogText("C’est ce qu’il s’est passé.")}, 17920);
+    setTimeout(()=>{setDialogText("Un soir de décembre, ils ont sonné.")}, 19550);
+    setTimeout(()=>{setDialogText("Nous nous sommes alors réfugiés dans notre cachette au grenier. ")}, 21760);
+    setTimeout(()=>{setDialogText("Mais ma mère n’a pas eu le temps de nous rejoindre lorsque la porte a été défoncée.")}, 24690);
+    setTimeout(()=>{setDialogText("Lorsqu’ils l’ont interrogée, elle a prétendu que mes frères, mon père et moi avions quitté la ville.")}, 28920);
+    setTimeout(()=>{setDialogText("Nous sommes restés cachés. ")}, 34570);
+    setTimeout(()=>{setDialogText("Elle nous a sauvé la vie.")}, 36840);
+    setTimeout(()=>{setDialogText("Puis elle a été emmenée.")}, 38190);
+    setTimeout(closeDialog, 41000);
+}
--- index.js
+++ index.js
@@ -1,11 +1,65 @@
-const CONSTANTS = {
+var CONSTANTS = {
     VIEW_SELECT: 1,
-    VIEW_CHAPTER: 2
+    VIEW_CHAPTER: 2,
+    CHAPTER_1: 3,
+    CHAPTER_2: 4,
+    CHAPTER_3: 5,
+    CHAPTER_4: 6,
+    HIDER_X_TRANSITION: "left 1200ms ease, width 1200ms ease",
+    HIDER_Y_TRANSITION: "none",
+    DIALOG_TEXT_HEIGHT: 23,
+    ANIMATION_DELAY: 210
 };
 
-window.onload = () => {
-    setChoicesEnabled(false);
+var PARAMETERS = {
+    FULLSCREEN: false
 }
+
+var SOUNDS = {
+    CHAPTER_1: {
+        NARATIVE: null,
+        BACKGROUND: null,
+        EFFECT_INDEX: 0,
+        EFFECTS: []
+    },
+    CHAPTER_2: {
+        NARATIVE: null,
+        BACKGROUND: null,
+        EFFECT_INDEX: 0,
+        EFFECTS: []
+    },
+    CHAPTER_3: {
+        NARATIVE: null,
+        BACKGROUND: null,
+        EFFECT_INDEX: 0,
+        EFFECTS: []
+    },
+    CHAPTER_4: {
+        NARATIVE: null,
+        BACKGROUND: null,
+        EFFECT_INDEX: 0,
+        EFFECTS: []
+    },
+}
+
+window.onload = () => {
+    loadRessources();
+    setChoicesEnabled(false);
+    changeViewTo(CONSTANTS.VIEW_SELECT);
+}
+
+window.addEventListener("keydown", ev => {
+    if (!PARAMETERS.FULLSCREEN)
+        openFullscreen(document.body);
+    console.log(ev.key)
+    if (ev.key == "Delete")
+        changeViewTo(CONSTANTS.VIEW_SELECT);
+})
+
+window.addEventListener("mousedown", ev => {
+    if (!PARAMETERS.FULLSCREEN)
+        openFullscreen(document.body);
+})
 
 /**
  * Change l'état de ma fenetre des choix
@@ -33,7 +87,7 @@ function setChoicesEnabled(state, title="Question", left="Choix 1", right="Choix
  * Change l'état de ma fenetre des choix
  * @param {number} view_number état de la fenetre des choix (true: ouverte, false: fermée)
  */
-function changeViewTo(view_number) {
+function changeViewTo(view_number, chapter=0) {
     let selectDiv = document.getElementById("select-container");
     let chaptersDiv = document.getElementById("chapters-container");
     switch (view_number) {
@@ -42,6 +96,23 @@ function changeViewTo(view_number) {
             chaptersDiv.style.transform = "scale(1)";
             selectDiv.style.opacity = "0";
             selectDiv.style.transform = "scale(0)";
+            switch (chapter) {
+                case CONSTANTS.CHAPTER_1:
+                    loadChapter(1);
+                    break;
+                case CONSTANTS.CHAPTER_2:
+                    loadChapter(2);
+                    break;
+                case CONSTANTS.CHAPTER_3:
+                    loadChapter(3);
+                    break;
+                case CONSTANTS.CHAPTER_4:
+                    loadChapter(4);
+                    break;
+            
+                default:
+                    break;
+            }
             break;
 
         case CONSTANTS.VIEW_SELECT:
@@ -54,4 +125,50 @@ function changeViewTo(view_number) {
         default:
             break;
     }
+}
+
+function playSound(path="") {
+    let a = new Audio()
+    a.src = path;
+    a.play();
+}
+
+function getMicVolume() {
+    
+}
+
+function openFullscreen(elem) {
+    if (elem.requestFullscreen) {
+        elem.requestFullscreen().then(()=>{
+            PARAMETERS.FULLSCREEN = true;
+        });
+    } else if (elem.webkitRequestFullscreen) { /* Safari */
+        elem.webkitRequestFullscreen().then(()=>{
+            PARAMETERS.FULLSCREEN = true;
+        });
+    } else if (elem.msRequestFullscreen) { /* IE11 */
+        elem.msRequestFullscreen().then(()=>{
+            PARAMETERS.FULLSCREEN = true;
+        });
+    }
+}
+  
+function closeFullscreen() {
+    if (document.exitFullscreen) {
+        document.exitFullscreen().then(()=>{
+            PARAMETERS.FULLSCREEN = false;
+        });
+    } else if (document.webkitExitFullscreen) { /* Safari */
+        document.webkitExitFullscreen().then(()=>{
+            PARAMETERS.FULLSCREEN = false;
+        });
+    } else if (document.msExitFullscreen) { /* IE11 */
+        document.msExitFullscreen().then(()=>{
+            PARAMETERS.FULLSCREEN = false;
+        });
+    }
+}
+
+function loadRessources() {
+    SOUNDS.CHAPTER_1.NARATIVE = new Audio("./resources/audio/chapter_1/narative.wav");
 }
